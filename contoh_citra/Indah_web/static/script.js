492bd8d7f5fd22c2fcdc3d9b6b19cf255ade926d
indahh241 <indonggds@gmail.com>
2025-03-10 22:45:49 +0700

    memperbarui script.js

--- contoh_citra/Indah_web/static/script.js
+++ contoh_citra/Indah_web/static/script.js
@@ -1,10 +1,10 @@
-document.getElementById("imageInput").addEventListener("change", function(event) {
+document.getElementById("imageInput").addEventListener("change", (event) => {
     const preview = document.getElementById("preview");
-    const file = event.target.files[0];
+    const [file] = event.target.files;
 
     if (file) {
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = (e) => {
             preview.src = e.target.result;
             preview.style.display = "block";
         };
